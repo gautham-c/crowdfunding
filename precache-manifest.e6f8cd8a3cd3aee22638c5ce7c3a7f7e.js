0e2527debc1d79346bd638c5aa07e780c9bd50db
gautham-c <gautham.chadalavada@gmail.com>
2022-04-10 11:57:46 +0530

    Updates

---
+++ precache-manifest.e6f8cd8a3cd3aee22638c5ce7c3a7f7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e399f7e337fe8d453841dfb07b1d2c",
+    "url": "/crowdfunding/index.html"
+  },
+  {
+    "revision": "eb2195082b03b6bc55c7",
+    "url": "/crowdfunding/static/css/main.ca476b48.chunk.css"
+  },
+  {
+    "revision": "3d02514da1de87a0ae9f",
+    "url": "/crowdfunding/static/js/2.20b5cae3.chunk.js"
+  },
+  {
+    "revision": "4ce7daf2d0ad5c05eb4711f46d0992bc",
+    "url": "/crowdfunding/static/js/2.20b5cae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2195082b03b6bc55c7",
+    "url": "/crowdfunding/static/js/main.0504af61.chunk.js"
+  },
+  {
+    "revision": "50e380bdb0c2015e0b58",
+    "url": "/crowdfunding/static/js/runtime-main.fbe725f5.js"
+  },
+  {
+    "revision": "d7b8ace10c9f2afbdadc98b6bdd58db1",
+    "url": "/crowdfunding/static/media/about.d7b8ace1.png"
+  },
+  {
+    "revision": "d81b5e55cebf92923392d22fc6b5e7db",
+    "url": "/crowdfunding/static/media/hero.d81b5e55.jpeg"
+  }
+]);
